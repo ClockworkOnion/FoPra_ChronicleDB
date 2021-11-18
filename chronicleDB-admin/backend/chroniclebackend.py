fba27a794019eaff18a34d89d8660056149e6118
ClockworkOnion <happella@students.uni-marburg.de>
2021-11-18 15:36:34 +0100

    Updated backend method names etc

--- chronicleDB-admin/backend/chroniclebackend.py
+++ chronicleDB-admin/backend/chroniclebackend.py
@@ -9,18 +9,27 @@ api = Api(app)
 cors = CORS(app) # Adding CORS header
 app.config['CORS_HEADERS'] = 'Content-Type' # Adding CORS header
 
-class ChronicleDB(Resource):
+class showRightFlank(Resource):
     def get(self):
-        response = requests.get("http://127.0.0.1:8000/show_right_flank/2")
+        print("Trying to get right flank from ChronicleDB at localhost:8000/show_right_flank/0 ... ")
+        response = requests.get("http://127.0.0.1:8000/show_right_flank/0")
+        print("Response from ChronicleDB: "+ str(response))
         return jsonify(response.json())
 
     def post(self):
-        print(request.data)
-        response = requests.post("http://127.0.0.1:8000/insert_ordered/2", request.data)
-        print("Response from ChronicleDB: " + str(response))
-        return {"data": "Posting success?" } 
+        return "Not defined!"
 
-api.add_resource(ChronicleDB, "/getright")
+class createStream(Resource):
+    def get(self):
+        return "Not defined!"
+
+    def post(self):
+        print(request.data)
+        response = requests.post("http://127.0.0.1:8000/create_stream", request.data)
+        print("Response from ChronicleDB: " + str(response))
+        return {"response": "Posting success?" } 
+
+api.add_resource(createStream, "/create_stream")
 
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
