4b8b206b20a7abd6a248dc8fae72700bb94f669a
ClockworkOnion <happella@students.uni-marburg.de>
2021-12-02 14:50:52 +0100

    VarCompounds funktionieren

--- chronicleDB-admin/src/app/components/show-right-flank/show-right-flank.component.ts
+++ chronicleDB-admin/src/app/components/show-right-flank/show-right-flank.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventElementType } from 'src/app/model/ChronicleEvent';
 import { ChronicleStream } from 'src/app/model/ChronicleStream';
 import { ChronicleService } from 'src/app/services/chronicle.service';
 import { GetFlankService } from 'src/app/services/rest services/get-flank.service';
@@ -9,57 +10,77 @@ import { GetFlankService } from 'src/app/services/rest services/get-flank.servic
   styleUrls: ['./show-right-flank.component.css']
 })
 export class ShowRightFlankComponent implements OnInit {
-  streamList :Array<ChronicleStream>=[];
+  selectedStream!: ChronicleStream;
 
-  ACTIVE_STREAM : number = 0 // will be made selectable by user later
-
-  constructor(private service:GetFlankService, private chronicle:ChronicleService) { }
+  constructor(private getFlankService:GetFlankService, private chronicle:ChronicleService) { }
   flankInfo: any;
   outputInfo: string = "";
 
   ngOnInit(): void {
-    this.chronicle.currentStreamList.subscribe((streamlist:any)=>this.streamList =streamlist)
-    this.streamList=this.chronicle.streamList; // JSON.parse(sessionStorage.getItem("streamList")!);
+    this.chronicle.selectedStream$.subscribe(stream => {
+      if (stream) {
+        this.selectedStream = stream;
+      }
+    })
   }
 
   showRightFlank(){
-    this.service.basicRightFlank().subscribe(response =>{
+    this.getFlankService.basicRightFlank().subscribe(response =>{
       this.flankInfo = response;
       console.log("### Info: ###")
       let json = JSON.parse(this.flankInfo);
       console.log(json);
 
       // Collect General Info Strings:
-      let brotherLeft = json[this.ACTIVE_STREAM].brother_left;
-      let brotherRight = json[this.ACTIVE_STREAM].brother_right;
+      let brotherLeft = json[this.selectedStream.id].brother_left;
+      let brotherRight = json[this.selectedStream.id].brother_right;
 
       this.outputInfo = ""; // Reset Stream Info
 
       // Add "Brother Info":
       this.outputInfo += "Brother left: " + brotherLeft + "\nBrother right: " + brotherRight + "\n";
       // Output Index, Timestamp and Payload
-      for (let i = 0; i < json[this.ACTIVE_STREAM].node_variant.ValueNode.data_array.length; i++) {
-        this.outputInfo += "\n Index: " + i
-          + " Timestamp: " + this.getTimeStampFromJSON(json, this.ACTIVE_STREAM, i)
-          + " Payload: " + this.getPayloadFromJSON(json, this.ACTIVE_STREAM, i, this.getPayloadTypeFromEvent(this.ACTIVE_STREAM, 0));
+      for (let i = 0; i < json[this.selectedStream.id].node_variant.ValueNode.data_array.length; i++) { // iterate through data array
+        for (let e = 0; e < this.selectedStream.event.length; e++) {
+          this.outputInfo += "\n Index: " + i
+            + " Timestamp: " + this.getTimeStampFromJSON(json, this.selectedStream.id, i)
+            + " Payload: " + this.getPayloadFromJSON(json, this.selectedStream.id, i, this.getPayloadTypeFromEvent(this.selectedStream.id, e), e) 
+            + " Type: " + this.getPayloadTypeFromEvent(this.selectedStream.id, e);
+        }
+        this.outputInfo += "\n"
       }
     });
   }
 
+
   debugGetStreamInfo() { 
     console.log("Stream Info:");
-    console.log(this.streamList);
-    console.log(this.streamList[this.ACTIVE_STREAM]);
+    console.log(this.selectedStream);
+    console.log(this.selectedStream.event);
+    this.selectedStream.event.forEach(element => {
+      console.log(element);
+    });
+    console.log(this.selectedStream.compoundType)
 
-    console.log("Event count:" + this.streamList[this.ACTIVE_STREAM].event.length);
 
     console.log("Payload Type:")
-    console.log(this.streamList[0].event[0].type);
-    console.log(this.streamList[0].event[0].subtype);
+    this.selectedStream.event.forEach(e => {
+      console.log(e.subtype)
+      console.log(e.type)
+    });
     console.log("Payload (stream 0, event 0) nach Methode: "+ this.getPayloadTypeFromEvent(0,0)); 
   }
 
-  getPayloadFromJSON(json: any, streamNo: number, ValueNodeNo: number, payloadType: string) : string {
+  getPayloadFromJSON(json: any, streamNo: number, ValueNodeNo: number, payloadType: string, eventNo?: number) : string {
+
+    // VarCompound Type:
+    if (typeof eventNo != 'undefined') {
+    return json[streamNo].node_variant.ValueNode.data_array[ValueNodeNo].payload.VarCompound[eventNo][this.getPayloadTypeFromEvent(this.selectedStream.id, eventNo)];
+    }
+    //return json[streamNo].node_variant.ValueNode.data_array[ValueNodeNo].payload.VarCompound[0].I8;
+    //return json[streamNo].node_variant.ValueNode.data_array[ValueNodeNo].payload.VarCompound[1].F32;
+
+    // Single Type:
     return json[streamNo].node_variant.ValueNode.data_array[ValueNodeNo].payload[payloadType];
   }
 
@@ -69,21 +90,21 @@ export class ShowRightFlankComponent implements OnInit {
 
   getPayloadTypeFromEvent(streamNo: number, eventNo: number) : string {
     let prefix = ""
-    switch (this.streamList[streamNo].event[eventNo].type) {
-      case "Integer":
+    switch (this.selectedStream.event[eventNo].type) {
+      case EventElementType.integer:
         prefix = "I";
         break;
-      case "Float":
+      case EventElementType.float:
         prefix = "F";
         break;
-      case "Unsigned":
+      case EventElementType.unsigned:
         prefix = "U";
         break;
-      default:
+      default: // leave prefix empty when type is string
         break;
     }
-    console.log("Payload type in stream no " + streamNo + ", event no " + eventNo + " is: " + prefix + this.streamList[streamNo].event[eventNo].subtype)
-  return prefix + this.streamList[streamNo].event[eventNo].subtype;
+    console.log("Payload type in stream no " + streamNo + ", event no " + eventNo + " is: " + prefix + this.selectedStream.event[eventNo].subtype)
+  return prefix + this.selectedStream.event[eventNo].subtype;
   }
 
 }
