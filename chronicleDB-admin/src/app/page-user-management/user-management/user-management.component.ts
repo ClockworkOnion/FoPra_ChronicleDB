3af012e93e5d48e172f777ccda2f7e250907953a
Davit Davtyan <davtyand@students-marburg.de>
2022-01-21 19:24:34 +0100

    working user management

--- chronicleDB-admin/src/app/page-user-management/user-management/user-management.component.ts
+++ chronicleDB-admin/src/app/page-user-management/user-management/user-management.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { BACKEND_URL } from './../../services/auth.service';
 import { ChronicleService } from './../../services/chronicle.service';
 import { User } from './../../model/User';
@@ -6,15 +7,21 @@ import {Component, ViewChild} from '@angular/core';
 import { MatTableDataSource} from '@angular/material/table';
 import{MatSort} from '@angular/material/sort';
 import {MatPaginator}from "@angular/material/paginator"
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.css']
 })
-export class UserManagementComponent {
-  displayedColumns = ['username','vorname','name', 'allowed_streams','allowed_insert_streams',"created","last_logged_in"];
+export class UserManagementComponent  {
+  displayedColumns = ['username','isAdmin','allowedStreams',"canCreateStreams"];
   dataSource!: MatTableDataSource<User>;
+  form!: FormGroup;
+  users :any;
+  userArray:User[]=[];
+  
+  
 
   @ViewChild(MatPaginator)paginator!: MatPaginator;
   @ViewChild(MatSort)sort!: MatSort;
@@ -26,6 +33,35 @@ export class UserManagementComponent {
     
     //});
   }
+  ngOnInit() {
+    // this.form = this.formBuilder.group({
+    //   albums: this.formBuilder.array([])
+    // });
+    // this._albumService.getAllAsFormArray().subscribe(albums => {
+    //   this.form.setControl('albums', albums);
+    // });
+    this.chronicleService.getHttp().get(BACKEND_URL+"/allusers",{responseType:"json"}).subscribe((response:any) => {
+      this.users = response;
+      for (let index = 0; index < (response.users).length; index++) {
+      console.log(response.users[index])
+      let newUser: User={
+        username:response.users[index].username,
+        isAdmin:response.users[index].isAdmin,
+        canCreateStreams:response.users[index].canCreateStreams,
+        allStreamsAllowed:response.users[index].allStreamsAllowed,
+        allowedStreams:response.users[index].allowedStreams,
+        canInsertAll:response.users[index].canInsertAll,
+        allowedInsertStreams:response.users[index].allowedInsertStreams
+
+
+
+      }
+      this.userArray.push(newUser)
+      }
+      this.dataSource = new MatTableDataSource<User>(this.userArray);
+     })
+     
+  }
 
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
@@ -33,15 +69,18 @@ export class UserManagementComponent {
     this.dataSource.filter = filterValue;
   }
   test(){
-    let res = (this.chronicleService.getHttp().get(BACKEND_URL+"/allusers").subscribe(response => console.log(response)))
+    console.log(this.userArray)
+    
+    
+
+    
+  
+   
+    
     
   }
+
 }
 
-/** Constants used to fill up our data base. */
-const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+
 
