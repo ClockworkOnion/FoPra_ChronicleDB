ace930018717490b52c2c8aff2eb7724e36adc15
Johannes Buder (buderj) <buderj@students.uni-marburg.de>
2022-01-27 13:16:39 +0100

    added Guard for user Management

--- chronicleDB-admin/src/app/app-routing.module.ts
+++ chronicleDB-admin/src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { SystemInfoComponent } from './page-system-info/system-info/system-info.component';
-import { TabMenuComponent } from './components/tab-menu/tab-menu.component';
 import { DashboardComponent } from './page-home/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -12,6 +11,7 @@ import { AuthGuard } from './services/guards/auth-guard.service';
 import { CreateAuthGuard } from './services/guards/create-auth-guard.service';
 import { MasterGuard } from './services/guards/master-guard.service';
 import { UserManagementComponent } from './page-user-management/user-management/user-management.component';
+import { AdminAuthGuard } from './services/guards/admin-auth-guard.service';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, canActivate: [MasterGuard], data: {guards: [AuthGuard]} },
@@ -19,9 +19,9 @@ const routes: Routes = [
   { path: 'no-access', component: NoAccessComponent },
   // {path:"insertData",component:InsertDataTabMenuComponent},
   { path: 'systemInfo', component: SystemInfoComponent },
-  { path: 'user_management', component: UserManagementComponent },
+  { path: 'user_management', component: UserManagementComponent, canActivate: [MasterGuard], data: {guards: [AuthGuard, AdminAuthGuard]} },
   { path: 'show_right_flank', component: ShowRightFlankComponent },
-  { path: 'time_travel', component: TimeTravelComponent },
+  // { path: 'time_travel', component: TimeTravelComponent },
   { path: 'create_stream', component: CreateStreamComponent, canActivate: [MasterGuard], data: {guards: [AuthGuard, CreateAuthGuard]} },
   { path: '**', redirectTo: '' }, // als letztes wird alles andere zu home weitergeleitet...
 ];
