972c9b0b446e506eb512b2978201bb1603fd4ee6
Johannes Buder (buderj) <buderj@students.uni-marburg.de>
2022-02-02 11:47:25 +0100

    Jobs sortieren möglich

--- chronicleDB-admin/src/app/page-jobs/page-jobs.component.ts
+++ chronicleDB-admin/src/app/page-jobs/page-jobs.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ChronicleJob } from '../model/ChronicleJob';
 import { JobService } from '../services/job.service';
@@ -10,8 +11,10 @@ import { JobService } from '../services/job.service';
   styleUrls: ['./page-jobs.component.css']
 })
 export class PageJobsComponent implements OnInit, AfterViewInit {
+  jobs: ChronicleJob[] = [];
   dataSource: MatTableDataSource<ChronicleJob> = new MatTableDataSource();
-  displayedColumns: string[] = ['info', "time", "nextRun", "interval", "options"];
+  displayedColumns: string[] = ['info', "startDate", "nextRun", "interval", "options"];
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private jobService: JobService) { }
@@ -22,6 +25,7 @@ export class PageJobsComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.jobService.userJobsBS$.subscribe(jobs => {
+      this.jobs = jobs;
       this.dataSource.data = jobs;
     })
   }
@@ -38,6 +42,35 @@ export class PageJobsComponent implements OnInit, AfterViewInit {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
     this.dataSource.filter = filterValue;
+  }
+
+  sortData(sort: Sort) {
+    const data = this.jobs.slice();
+    if (!sort.active || sort.direction === '') {
+      this.dataSource.data = data;
+      return;
+    }
+    this.dataSource.data = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'startDate':
+          return compare(a.startDate.getTime(), b.startDate.getTime(), isAsc);
+        case 'nextRun':
+          return compare(a.nextRun.getTime(), b.nextRun.getTime(), isAsc);
+        case 'interval':
+          return compare(a.interval.value, b.interval.value, isAsc);
+        case 'info':
+          const aValue = a.info || a.requestType;
+          const bValue = b.info || b.requestType;
+          return compare(aValue.toLocaleLowerCase(), bValue.toLocaleLowerCase(), isAsc);
+        default:
+          return 0;
+      }
+    });
+
+    function compare(a: number | string, b: number | string, isAsc: boolean) {
+      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    }
   }
 
 }
